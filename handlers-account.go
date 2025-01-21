6c05cfc111de64bf8b4b900f9fd6fdbf0d331668
lukasWastaken <l.leutgeb@students.htl-leonding.ac.at>
2025-01-21 07:26:27 +0100

    Added handlers for POST method login and register

--- handlers-account.go
+++ handlers-account.go
@@ -6,9 +6,32 @@ import (
 )
 
 func POSTregisterHandler(c *gin.Context){
+	email := c.Param("email")
+	username := c.Param("username")
+	password := c.Param("password")
+	success, msg := register(email, username, password)
 
+	if !success{
+		c.String(500,msg)
+	}else{
+		c.String(201, msg)
+	}
 }
 
 func POSTloginHandler(c *gin.Context){
-	
+	email := c.Param("email")
+	password := c.Param("password")
+
+	loginReq := LoginRequest {
+		Email: email,
+		Password: password,
+	}
+
+	success, errMSG := login(loginReq)
+
+	if !success {
+		c.String(500, errMSG)
+	}else{
+		c.String(200, errMSG)
+	}
 }
