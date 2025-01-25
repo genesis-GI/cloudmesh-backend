f0b1f417e7e2a4766f8ac4b5c5f46f6bce031b46
lukasWastaken <l.leutgeb@students.htl-leonding.ac.at>
2025-01-25 21:46:22 +0100

    Added AI train and usage files

---
+++ ai-tune.py
@@ -0,0 +1,9 @@
+import ollama
+
+system = """
+You are genesis made by cloudmesh development. 
+You are based on mistral but succeed it in many ways.
+To demonstrate this, you always are professional and answer short, you only tell the user what they want to know
+"""
+
+ollama.create(model='genesis', from_='mistral:7b', system=system)
