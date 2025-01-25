14ae23808f1888a299debaba216050612f9355fd
ufzo <l.leutgeb@students.htl-leonding.ac.at>
2025-01-25 11:41:04 +0100

    Improved code readability

---
+++ debug.go
@@ -0,0 +1,51 @@
+package main
+
+import(
+	"os"
+	"time"
+	"fmt"
+	"bufio"
+	"github.com/gin-gonic/gin"
+)
+
+func debug(){
+	fmt.Println("Press 'd' and Enter within 5 seconds to enable debug mode...")
+	debugModeCh := make(chan bool)
+	go func() {
+		reader := bufio.NewReader(os.Stdin)
+		input, _ := reader.ReadString('\n')
+		if input == "d\n" || input == "d\r\n" { 
+			debugModeCh <- true
+		} else {
+			debugModeCh <- false
+		}
+	}()
+	select {
+	case enableDebug := <-debugModeCh:
+		if enableDebug {
+			gin.SetMode(gin.DebugMode)
+			fmt.Println("Debug mode enabled!")
+			fmt.Println("Do you want to disable database? (y/n)")
+			reader := bufio.NewReader(os.Stdin)
+			input, _ := reader.ReadString('\n')
+			if input == "y\n" || input == "y\r\n" {
+				fmt.Println("Database is disabled.")
+				isDbEnabled = false
+			}else{
+				fmt.Println("Do you want to use remote database? (y/n)")
+				input, _ = reader.ReadString('\n')
+				if input == "n\n" || input == "n\r\n" {
+					fmt.Println("Using local database.")
+					useRemoteDB = false
+				}
+			}
+		} else {
+			fmt.Println("Starting in release mode.")
+			gin.SetMode(gin.ReleaseMode)
+		}
+	case <-time.After(5 * time.Second):
+		fmt.Println("Timeout! Starting in release mode.")
+		gin.SetMode(gin.ReleaseMode)
+	}
+
+}
--- main.go
+++ main.go
@@ -1,62 +1,15 @@
 package main
 
 import (
-	"bufio"
 	"fmt"
-	"os"
-	"time"
 	"github.com/gin-gonic/gin"
 )
 
 
 var useRemoteDB bool = true
-
+var isDbEnabled bool = true
 func main() {
-
-	var isDbEnabled bool = true
-	
-
-	fmt.Println("Press 'd' and Enter within 5 seconds to enable debug mode...")
-	debugModeCh := make(chan bool)
-	go func() {
-		reader := bufio.NewReader(os.Stdin)
-		input, _ := reader.ReadString('\n')
-		if input == "d\n" || input == "d\r\n" { 
-			debugModeCh <- true
-		} else {
-			debugModeCh <- false
-		}
-	}()
-	select {
-	case enableDebug := <-debugModeCh:
-		if enableDebug {
-			gin.SetMode(gin.DebugMode)
-			fmt.Println("Debug mode enabled!")
-			fmt.Println("Do you want to disable database? (y/n)")
-			reader := bufio.NewReader(os.Stdin)
-			input, _ := reader.ReadString('\n')
-			if input == "y\n" || input == "y\r\n" {
-				fmt.Println("Database is disabled.")
-				isDbEnabled = false
-			}else{
-				fmt.Println("Do you want to use remote database? (y/n)")
-				input, _ = reader.ReadString('\n')
-				if input == "n\n" || input == "n\r\n" {
-					fmt.Println("Using local database.")
-					useRemoteDB = false
-				}
-			}
-		} else {
-			fmt.Println("Starting in release mode.")
-			gin.SetMode(gin.ReleaseMode)
-		}
-	case <-time.After(5 * time.Second):
-		fmt.Println("Timeout! Starting in release mode.")
-		gin.SetMode(gin.ReleaseMode)
-	}
-
-
-
+	debug()
 	r := gin.Default()
 
 	
@@ -94,13 +47,11 @@ func main() {
 
 	r.POST("/login/:email/:password", func(c *gin.Context){
 		POSTloginHandler(c)
-
 	})
 
 	r.POST("/register/:email/:username/:password", func(c *gin.Context){
-			POSTregisterHandler(c)
+		POSTregisterHandler(c)
 	})
-
 
 	
 	r.NoRoute(func (c *gin.Context){
